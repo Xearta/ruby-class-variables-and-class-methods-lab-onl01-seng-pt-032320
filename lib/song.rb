f8ac414b25067eefe0c891c0623fe4e5c43f649c
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 16:03:10 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,44 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+  @@count = 0
+  @@artists = []
+  @@genres = []
+  @@genre_count = {}
+  @@artist_count = {}
+  
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    @@genres.uniq
+  end
+  
+  def self.artists
+    @@artists.uniq
+  end
+  
+  def self.genre_count
+    @@genres.each do |genre|
+      if @@genre_count.key?(genre)
+        @@genre_count[:genre] += 1
+        
+      else
+        @@genre_count[:genre] = 1
+        
+      end
+    end
+  end
+  
+end
