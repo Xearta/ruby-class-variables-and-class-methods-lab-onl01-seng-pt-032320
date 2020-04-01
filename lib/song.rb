973f3e458dc5ec28880e629c250fd52256463161
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 16:04:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,12 +33,11 @@ class Song
     @@genres.each do |genre|
       if @@genre_count.key?(genre)
         @@genre_count[:genre] += 1
-        
       else
         @@genre_count[:genre] = 1
-        
       end
     end
+    @@genre_count
   end
   
 end
