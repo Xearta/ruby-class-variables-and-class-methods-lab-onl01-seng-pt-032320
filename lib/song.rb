d5fe407ca6649a4a6b1a5bee5e42a14fcca72ded
Tanner Townsend <rsmcwow@gmail.com>
2020-04-01 16:05:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,11 @@ class Song
   
   def self.artist_count
     @@artists.each do |artist|
-      
+      if @@artist_count.key?(artist)
+        @@artist_count[artist] += 1
+      else
+        @@artist_count[artist] = 1
+      end
     end
     @@artist_count
   end
